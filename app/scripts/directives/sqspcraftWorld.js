45fcfd2d7f5ea6ae4c94650955af539f22188370
Ryan Gee <ryan.cv.gee@gmail.com>
2013-05-20 16:17:51 -0400

    Create the renderer globally now...this should be a service?

--- app/scripts/directives/sqspcraftWorld.js
+++ app/scripts/directives/sqspcraftWorld.js
@@ -1,17 +1,29 @@
 'use strict';
 
+var renderer = new THREE.WebGLRenderer();
+
+var w = 800;
+var h = 600;
+renderer.setSize(w, h);
+
+var camera = new THREE.PerspectiveCamera(45, w/h, 0.1, 10000);
+
+var scene = new THREE.Scene();
+scene.add(camera);
+
+camera.position.z = 300;
+
+
 angular.module('hackdayVizApp')
   .directive('sqspcraftWorld', function () {
     return {
-      template: '<div></div>',
+      template: '<div><p>SQSP world for: {{site}}</p></div>',
       restrict: 'E',
       scope: {
         site: '@site'
       },
       link: function postLink(scope, element, attrs) {
-        scope.$watch('site', function() {
-          element.text('SQSP world for: ' + scope.site);
-        });
+        element.find('div').append(renderer.domElement);
       }
     };
   });
