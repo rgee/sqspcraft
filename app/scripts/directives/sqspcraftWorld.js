e0ef8b92eb261b31f452be3533e3c76a8189f509
Ryan Gee <ryan.cv.gee@gmail.com>
2013-05-20 16:28:38 -0400

    Got a stupid little cube rendering...soon no more angular to deal with.

--- app/scripts/directives/sqspcraftWorld.js
+++ app/scripts/directives/sqspcraftWorld.js
@@ -13,6 +13,18 @@ scene.add(camera);
 
 camera.position.z = 300;
 
+var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xCC0000 });
+var cube = new THREE.Mesh(new THREE.CubeGeometry(50, 50, 50, 26, 26), cubeMaterial);
+scene.add(cube);
+
+var pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.x = 10;
+pointLight.position.y = 50;
+pointLight.position.z = 130;
+
+scene.add(pointLight);
+
+var rendering = false;
 
 angular.module('hackdayVizApp')
   .directive('sqspcraftWorld', function () {
@@ -24,6 +36,17 @@ angular.module('hackdayVizApp')
       },
       link: function postLink(scope, element, attrs) {
         element.find('div').append(renderer.domElement);
+        scope.$watch('site', function() {
+          if (scope.site && !rendering) {
+            rendering = true;
+            (function renderLoop(){
+              requestAnimationFrame(renderLoop);
+              cube.rotation.y += 0.005;
+              cube.rotation.x += 0.009;
+              renderer.render(scene, camera);
+            })();
+          }
+        });
       }
     };
   });
